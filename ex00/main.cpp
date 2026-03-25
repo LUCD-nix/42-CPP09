85ebfd3d7146476846adcaec3d8e79d579036a03
Lucas Dupuy <lucascordu@gmail.com>
2026-03-25 12:10:12 +0100

    ex00: fleshed out main, needs testing/linting

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include "BitCoinExchange.hpp"
 
-int	main(void)
+int	main(int argc, char* argv[])
 {
-	BitCoinExchange bce("input.txt", "data.csv");
-	bce.initDatabase();
-	bce.processInput();
-	std::cout << "Success!";
+  if (argc != 2)
+	{
+	  std::cerr << "Error, must provide input file!" << std::endl;
+	  return (1);
+	}
+  std::string input_file_name(argv[1]);
+  BitCoinExchange	bce(input_file_name, "data.csv");
+  if (!bce.initDatabase())
+	{
+	  return (1);
+	}
+  bce.processInput();
+  std::cout << "Success!";
 }
-
