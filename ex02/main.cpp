cdef48a442e61105bf019d2791a64e0d42ff5a02
Lucas Dupuy <lucascordu@gmail.com>
2026-03-31 14:22:56 +0200

    ex02 : added timing

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,17 +1,38 @@
 #include "PmergeMe.hpp"
 #include <iostream>
+#include <iomanip>
+#include <sys/time.h>
 
 int	main(int argc, char* argv[])
 {
+	timeval start, end;
+	double elapsed;
+
+	// INGEST VECTOR
+	gettimeofday(&start, NULL);
 	if (!PmergeMe::vecIngestInput(argc, argv))
 	{
 		return (1);
 	}
+	gettimeofday(&end, NULL);
+	elapsed = end.tv_sec - start.tv_sec + (end.tv_usec - start.tv_usec)
+												 / 1e6;
+	std::cout << std::setprecision(6)
+		<< "Time to ingest data into vector : " << elapsed << "s" << std::endl;
+
+	// SORT VECTOR
+	gettimeofday(&start, NULL);
 	if (!PmergeMe::sortVector())
 	{
 		std::cerr << "Error, vec not sorted" << std::endl;
 		return (1);
 	}
+	gettimeofday(&end, NULL);
+	elapsed = end.tv_sec - start.tv_sec + (end.tv_usec - start.tv_usec)
+												 / 1e6;
+	std::cout << std::setprecision(6)
+		<< "Time to sort vector :" << elapsed << "s" << std::endl;
+
 	PmergeMe::printVector();
 	std::cout << "Success!" << std::endl;
 }
